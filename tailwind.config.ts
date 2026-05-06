1b2f3ab0cfcdfe496e7576af3c4128fe884e1fbc
Kilua Zoldick <nangadoumer@gmail.com>
2026-05-06 14:13:32 +0100

    modification pour la correction du deploiement vercel avec quelques retouches

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: ["class"],
+  darkMode: ["class", "dark"],
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
